253d4ba1397e5bec759a0874dae958cc030e2611
Dubasov <Dubasovec@gmail.com>
2024-12-09 11:57:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#1st program
+print('1st program:')
+print(9**0.5*5)
+#2st program
+print('2st program:')
+print(9.99>9.98 and 1000!=1000.1)
+#3st program
+print('3st program:')
+print(2*2+2)
+print(2*(2+2))
+print('==:', (2*2+2)==(2*(2+2)))
+print('>:', (2*2+2)>(2*(2+2)))
+print('<:', (2*2+2)<(2*(2+2)))
+print('!=:', (2*2+2)!=(2*(2+2)))
+print('>=:', (2*2+2)>=(2*(2+2)))
+print('<=:', (2*2+2)<=(2*(2+2)))
+#4st program
+print('4st program:')
+exam = '123.456'
+print(int(float(exam)*10)%10)
